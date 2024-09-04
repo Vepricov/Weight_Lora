05474abd225e0fec13387a50a7f7b337a39cabbd
Vepricov <veprikov.as@phystech.edu>
2024-09-04 21:23:09 +0300

    add more optimizers

--- pipelines/optimizers.py
+++ pipelines/optimizers.py
@@ -1,7 +1,18 @@
-import torch.optim as optim
-from typing import Callable
 import torch
+import math
 import numpy as np
+import torch.optim as optim
+import warnings
+from typing import Callable
+
+############################### torch optimizers ###############################
+# LAMB optimizer: torch_optimizer.Lamb(model.parameters(), lr=0.001)
+# LAMB scheduler: vanilla linear (?) 
+# Prodigy optimizer: prodigyopt.Prodigy(model.parameters(), lr=1.)
+# Prodijy scheduler: torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=max_steps)
+# DAdaptAdam optimizer: dadaptation.DAdaptAdam(model.parameters(), lr=1.)
+# DAdaptAdam scheduler: same as for Prodijy optimizer (?)
+################################################################################
 
 # from https://github.com/jxbz/signSGD/blob/master/signSGD_zeros.ipynb
 class signSGD(optim.Optimizer):
@@ -34,135 +45,199 @@ class signSGD(optim.Optimizer):
         return loss
     
 class signAdamW(optim.Optimizer):
-    r"""Implements signAdamW algorithm.
-
-    Arguments:
-        params: iterable of parameters to optimize or dicts defining
-            parameter groups
-        lr: learning rate (default: 1e-3)
-        betas: coefficients used for computing
-            running averages of gradient and its square (default: (0.9, 0.999))
-        eps: term added to the denominator to improve
-            numerical stability (default: 1e-8)
-        weight_decay: weight decay (L2 penalty) (default: 0)
-        delta: threhold that determines whether a set of parameters is scale
-            invariant or not (default: 0.1)
-        wd_ratio: relative weight decay applied on scale-invariant parameters
-            compared to that applied on scale-variant parameters (default: 0.1)
-        nesterov: enables Nesterov momentum (default: False)
-
-    Note:
-        Reference code: https://pytorch-optimizer.readthedocs.io/en/latest/_modules/torch_optimizer/adamp.html#AdamP
-    """
-
     def __init__(
         self,
         params,
         lr: float = 1e-3,
-        betas: tuple = (0.9, 0.999),
-        eps: float = 1e-8,
-        weight_decay: float = 0,
-        delta: float = 0.1,
-        wd_ratio: float = 0.1,
-        nesterov: bool = False,
-        rand_zero = True,
-    ) -> None:
-        if lr <= 0.0:
-            raise ValueError('Invalid learning rate: {}'.format(lr))
-        if eps < 0.0:
-            raise ValueError('Invalid epsilon value: {}'.format(eps))
+        betas: tuple[float, float] = (0.9, 0.999),
+        eps: float = 1e-6,
+        weight_decay: float = 0.0,
+        correct_bias: bool = True,
+        no_deprecation_warning: bool = True,
+        rand_zero: bool = True
+    ):
+        if not no_deprecation_warning:
+            warnings.warn(
+                FutureWarning,
+            )
+        # require_version("torch>=1.5.0")  # add_ with alpha
+        if lr < 0.0:
+            raise ValueError(f"Invalid learning rate: {lr} - should be >= 0.0")
         if not 0.0 <= betas[0] < 1.0:
-            raise ValueError(
-                'Invalid beta parameter at index 0: {}'.format(betas[0])
-            )
+            raise ValueError(f"Invalid beta parameter: {betas[0]} - should be in [0.0, 1.0)")
         if not 0.0 <= betas[1] < 1.0:
-            raise ValueError(
-                'Invalid beta parameter at index 1: {}'.format(betas[1])
-            )
-        if weight_decay < 0:
-            raise ValueError(
-                'Invalid weight_decay value: {}'.format(weight_decay)
-            )
-        if delta < 0:
-            raise ValueError('Invalid delta value: {}'.format(delta))
-        if wd_ratio < 0:
-            raise ValueError('Invalid wd_ratio value: {}'.format(wd_ratio))
-
-        defaults = dict(
-            lr=lr,
-            betas=betas,
-            eps=eps,
-            weight_decay=weight_decay,
-            delta=delta,
-            wd_ratio=wd_ratio,
-            nesterov=nesterov,
-        )
+            raise ValueError(f"Invalid beta parameter: {betas[1]} - should be in [0.0, 1.0)")
+        if not 0.0 <= eps:
+            raise ValueError(f"Invalid epsilon value: {eps} - should be >= 0.0")
+        defaults = {"lr": lr, "betas": betas, "eps": eps, "weight_decay": weight_decay, "correct_bias": correct_bias}
         self.rand_zero = rand_zero
-        super(signAdamW, self).__init__(params, defaults)
+        super().__init__(params, defaults)
 
-    def step(self, closure=None):
-        r"""Performs a single optimization step.
-
-        Arguments:
-            closure: A closure that reevaluates the model and returns the loss.
-        """
+    @torch.no_grad()
+    def step(self, closure: Callable = None):
         loss = None
         if closure is not None:
             loss = closure()
 
         for group in self.param_groups:
-            for p in group['params']:
+            for p in group["params"]:
                 if p.grad is None:
                     continue
-
                 ########################## sign step ###########################
-                # OLD: grad = p.grad.data
+                # OLD: grad = p.grad
                 grad = torch.sign(p.grad)
                 # randomise zero gradients to ±1
                 if self.rand_zero:
                     grad[grad==0] = torch.randint_like(grad[grad==0], low=0, high=2)*2 - 1
                     assert not (grad==0).any()
                 ################################################################
-
-                beta1, beta2 = group['betas']
-                nesterov = group['nesterov']
+                if grad.is_sparse:
+                    raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
 
                 state = self.state[p]
 
                 # State initialization
                 if len(state) == 0:
-                    state['step'] = 0
-                    state['exp_avg'] = torch.zeros_like(p.data)
-                    state['exp_avg_sq'] = torch.zeros_like(p.data)
+                    state["step"] = 0
+                    # Exponential moving average of gradient values
+                    state["exp_avg"] = torch.zeros_like(p)
+                    # Exponential moving average of squared gradient values
+                    state["exp_avg_sq"] = torch.zeros_like(p)
 
-                # Adam
-                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                exp_avg, exp_avg_sq = state["exp_avg"], state["exp_avg_sq"]
+                beta1, beta2 = group["betas"]
 
-                state['step'] += 1
-                bias_correction1 = 1 - beta1 ** state['step']
-                bias_correction2 = 1 - beta2 ** state['step']
+                state["step"] += 1
 
-                exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
-                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
+                # Decay the first and second moment running average coefficient
+                # In-place operations to update the averages at the same time
+                exp_avg.mul_(beta1).add_(grad, alpha=(1.0 - beta1))
+                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1.0 - beta2)
+                denom = exp_avg_sq.sqrt().add_(group["eps"])
 
-                denom = (exp_avg_sq.sqrt() / np.sqrt(bias_correction2)).add_(
-                    group['eps']
-                )
-                step_size = group['lr'] / bias_correction1
+                step_size = group["lr"]
+                if group["correct_bias"]:  # No bias correction for Bert
+                    bias_correction1 = 1.0 - beta1 ** state["step"]
+                    bias_correction2 = 1.0 - beta2 ** state["step"]
+                    step_size = step_size * math.sqrt(bias_correction2) / bias_correction1
 
-                if nesterov:
-                    perturb = (beta1 * exp_avg + (1 - beta1) * grad) / denom
-                else:
-                    perturb = exp_avg / denom
+                p.addcdiv_(exp_avg, denom, value=-step_size)
 
-                # Weight decay
-                if group['weight_decay'] > 0:
-                    p.data.mul_(
-                        1 - group['lr'] * group['weight_decay'] * group['wd_ratio']
-                    )
+                if group["weight_decay"] > 0.0:
+                    p.add_(p, alpha=(-group["lr"] * group["weight_decay"]))
 
-                # Step
-                p.data.add_(perturb, alpha=-step_size)
+        return loss
+    
+class AdamW(optim.Optimizer):
+    """
+    Implements Adam algorithm with weight decay fix as introduced in [Decoupled Weight Decay
+    Regularization](https://arxiv.org/abs/1711.05101).
+
+    Parameters:
+        params (`Iterable[nn.parameter.Parameter]`):
+            Iterable of parameters to optimize or dictionaries defining parameter groups.
+        lr (`float`, *optional*, defaults to 0.001):
+            The learning rate to use.
+        betas (`Tuple[float,float]`, *optional*, defaults to `(0.9, 0.999)`):
+            Adam's betas parameters (b1, b2).
+        eps (`float`, *optional*, defaults to 1e-06):
+            Adam's epsilon for numerical stability.
+        weight_decay (`float`, *optional*, defaults to 0.0):
+            Decoupled weight decay to apply.
+        correct_bias (`bool`, *optional*, defaults to `True`):
+            Whether or not to correct bias in Adam (for instance, in Bert TF repository they use `False`).
+        no_deprecation_warning (`bool`, *optional*, defaults to `False`):
+            A flag used to disable the deprecation warning (set to `True` to disable the warning).
+    """
+
+    def __init__(
+        self,
+        params,
+        lr: float = 1e-3,
+        betas: tuple[float, float] = (0.9, 0.999),
+        eps: float = 1e-6,
+        weight_decay: float = 0.0,
+        correct_bias: bool = True,
+        no_deprecation_warning: bool = False,
+    ):
+        if not no_deprecation_warning:
+            warnings.warn(
+                "This implementation of AdamW is deprecated and will be removed in a future version. Use the PyTorch"
+                " implementation torch.optim.AdamW instead, or set `no_deprecation_warning=True` to disable this"
+                " warning",
+                FutureWarning,
+            )
+        # require_version("torch>=1.5.0")  # add_ with alpha
+        if lr < 0.0:
+            raise ValueError(f"Invalid learning rate: {lr} - should be >= 0.0")
+        if not 0.0 <= betas[0] < 1.0:
+            raise ValueError(f"Invalid beta parameter: {betas[0]} - should be in [0.0, 1.0)")
+        if not 0.0 <= betas[1] < 1.0:
+            raise ValueError(f"Invalid beta parameter: {betas[1]} - should be in [0.0, 1.0)")
+        if not 0.0 <= eps:
+            raise ValueError(f"Invalid epsilon value: {eps} - should be >= 0.0")
+        defaults = {"lr": lr, "betas": betas, "eps": eps, "weight_decay": weight_decay, "correct_bias": correct_bias}
+        super().__init__(params, defaults)
+
+    @torch.no_grad()
+    def step(self, closure: Callable = None):
+        """
+        Performs a single optimization step.
+
+        Arguments:
+            closure (`Callable`, *optional*): A closure that reevaluates the model and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                grad = p.grad
+                if grad.is_sparse:
+                    raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
+
+                state = self.state[p]
+
+                # State initialization
+                if len(state) == 0:
+                    state["step"] = 0
+                    # Exponential moving average of gradient values
+                    state["exp_avg"] = torch.zeros_like(p)
+                    # Exponential moving average of squared gradient values
+                    state["exp_avg_sq"] = torch.zeros_like(p)
+
+                exp_avg, exp_avg_sq = state["exp_avg"], state["exp_avg_sq"]
+                beta1, beta2 = group["betas"]
+
+                state["step"] += 1
+
+                # Decay the first and second moment running average coefficient
+                # In-place operations to update the averages at the same time
+                exp_avg.mul_(beta1).add_(grad, alpha=(1.0 - beta1))
+                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1.0 - beta2)
+                denom = exp_avg_sq.sqrt().add_(group["eps"])
+
+                step_size = group["lr"]
+                if group["correct_bias"]:  # No bias correction for Bert
+                    bias_correction1 = 1.0 - beta1 ** state["step"]
+                    bias_correction2 = 1.0 - beta2 ** state["step"]
+                    step_size = step_size * math.sqrt(bias_correction2) / bias_correction1
+
+                p.addcdiv_(exp_avg, denom, value=-step_size)
+
+                # Just adding the square of the weights to the loss function is *not*
+                # the correct way of using L2 regularization/weight decay with Adam,
+                # since that will interact with the m and v parameters in strange ways.
+                #
+                # Instead we want to decay the weights in a manner that doesn't interact
+                # with the m/v parameters. This is equivalent to adding the square
+                # of the weights to the loss with plain (non-momentum) SGD.
+                # Add weight decay at the end (fixed version)
+                if group["weight_decay"] > 0.0:
+                    p.add_(p, alpha=(-group["lr"] * group["weight_decay"]))
 
         return loss
 
@@ -183,10 +258,6 @@ class SGD(optim.Optimizer):
                     continue
                 p.data -= group['lr'] * p.grad.data
         return loss
-    
-############################### torch optimizers ###############################
-# LAMB : torch_optimizer.Lamb(params, lr=0.001)
-################################################################################
     
 class StoIHT(optim.Optimizer):
     def __init__(self, params, k, approx, proj, prob, lr=0.01):
--- pipelines/unsloth_qlora_finetune.py
+++ pipelines/unsloth_qlora_finetune.py
@@ -1,5 +1,6 @@
 import os
-import torch_optimizer
+# import torch_optimizer
+import torch
 from unsloth import FastLanguageModel
 from unsloth.chat_templates import get_chat_template
 from unsloth import is_bfloat16_supported
@@ -29,16 +30,18 @@ class TrainingArguments(TrainingArguments):
     gradient_accumulation_steps: int = 4
     warmup_steps: int = 5
     num_train_epochs: int = 5
-    learning_rate: float = 5e-5
+    learning_rate: float = 1e-1
     fp16: bool = not is_bfloat16_supported()
     bf16: bool = is_bfloat16_supported()
     logging_steps: int = 1
-    # optim: str = "rmsprop"
+    optim: str = "adamw_hf"
     weight_decay: float = 0.01
     lr_scheduler_type: str = "linear"
     seed: int = 18
     output_dir: str = "train_outputs"
     # output_dir: str = None
+    sign_step: int = 5000
+    max_grad_norm: float = 1.0
     max_steps: int = 50 # overrides num_train_epochs
     report_to: str = "wandb" # "none" or "wandb"
  
@@ -47,8 +50,11 @@ class DataArguments:
     train_file: str = 'data/train_ft_short_system.jsonl'
  
 def main():
-    parser = HfArgumentParser((ModelArguments, TrainingArguments, DataArguments))
-    model_args, training_args, data_args = parser.parse_args_into_dataclasses()
+    # parser = HfArgumentParser((ModelArguments, TrainingArguments, DataArguments))
+    # model_args, training_args, data_args = parser.parse_args_into_dataclasses()
+    model_args = ModelArguments
+    training_args = TrainingArguments
+    data_args = DataArguments
 
     utils.set_seed(training_args.seed)
 
@@ -89,15 +95,28 @@ def main():
     
     dataset = train_dataset.map(formatting_prompts_func, batched=True)
 
+    optimizer, scheduler = None, None
     ######################### Optimizer and Scheduler ##########################
-    optimizer = optimizers.signAdamW(model.parameters(), 
-                                     lr=training_args.learning_rate,
-                                     weight_decay=training_args.weight_decay)
-    scheduler = get_scheduler(name=training_args.lr_scheduler_type, 
-                              optimizer=optimizer, 
-                              num_warmup_steps=training_args.warmup_steps,
-                              num_training_steps=training_args.max_steps)
+    # optimizer = optimizers.signAdamW(model.parameters(), 
+    #                                  lr=training_args.learning_rate,
+    #                                  weight_decay=training_args.weight_decay)
+    # scheduler = get_scheduler(name=training_args.lr_scheduler_type, 
+    #                           optimizer=optimizer, 
+    #                           num_warmup_steps=training_args.warmup_steps,
+    #                           num_training_steps=training_args.max_steps)
+    import dadaptation
+    optimizer = dadaptation.DAdaptAdam(model.parameters(), 
+                                       lr=training_args.learning_rate,
+                                       weight_decay=training_args.weight_decay)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 
+                                                           T_max=training_args.max_steps)
     ############################################################################
+
+    if training_args.report_to == "wandb":
+        if optimizer is not None:
+            run_name=optimizer.__class__.__name__
+        else:
+            run_name=training_args.optim
     
     os.environ["WANDB_PROJECT"] = "SBER_LORA"
     trainer = SFTTrainer(
@@ -117,19 +136,33 @@ def main():
             fp16=training_args.fp16,
             bf16=training_args.bf16,
             logging_steps=training_args.logging_steps,
-            # optim=optimizer.__class__.__name__,
+            optim=training_args.optim,
             weight_decay=training_args.weight_decay,
             lr_scheduler_type=training_args.lr_scheduler_type,
             seed=training_args.seed,
             output_dir=training_args.output_dir,
             max_steps=training_args.max_steps,
             report_to=training_args.report_to,
-            run_name=optimizer.__class__.__name__
+            run_name=run_name,
+            max_grad_norm=training_args.max_grad_norm
         ),
         optimizers=[optimizer, scheduler]
     )
 
     trainer_stats = trainer.train()
+
+    if training_args.report_to == "wandb":
+        import wandb
+        if optimizer is not None:
+            add_wandb_config = {"optimizer" : optimizer.__class__.__name__,
+                                "scheduler" : scheduler.__class__.__name__,
+                                }
+        else:
+            add_wandb_config = {"optimizer" : training_args.optim,
+                                "scheduler" : training_args.lr_scheduler_type,
+                                }
+
+        wandb.config.update(add_wandb_config)
 
 if __name__ == "__main__":
     main()
